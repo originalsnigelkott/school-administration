cb5c596e739aa18e75eab88f92bf14c7404c8d47
Per Dahlstedt <dahlstedt.per@gmail.com>
2020-09-27 10:34:17 +0200

    Changed method visability

--- Server.js
+++ Server.js
@@ -2,13 +2,11 @@ const express = require("express");
 const app = express();
 
 module.exports = class Server {
-  constructor() {}
-
   run() {
-    startServer();
+    this.#startServer();
   }
 
-  startServer() {
+  #startServer() {
     const serverPort = 3060;
     app.listen(serverPort, () => {
       console.log(`Listening on port ${serverPort}`);
