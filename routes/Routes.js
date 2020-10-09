15e453c4f103791a9a5f622ba3ed374a05e8d36d
Per Dahlstedt <dahlstedt.per@gmail.com>
2020-10-09 10:26:59 +0200

    Added teacher routes to router

--- routes/Routes.js
+++ routes/Routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const studentRoutes = require("./StudentRoutes");
+const teacherRoutes = require("./TeacherRoutes");
 
 router.use("/students", studentRoutes);
+router.use("/teachers", teacherRoutes);
 
-router.get("/", (req, res, next) => {
+router.get("/", (req, res) => {
   res.json("Hello, world!");
 });
 
