24a80ed2081cfea5884e9fcfc70596cf97afaa13
Per Dahlstedt <dahlstedt.per@gmail.com>
2020-10-09 16:14:34 +0200

    Added create function and updated responses

--- routes/BaseRouteFunctions.js
+++ routes/BaseRouteFunctions.js
@@ -13,13 +13,19 @@ module.exports = class BaseRouteFunctions {
   static update = async (req, res, service) => {
     const id = req.params.id;
     const updates = req.body;
-    await service.update(id, updates);
-    res.status(204)
+    service.update(id, updates);
+    res.sendStatus(204);
   }
 
   static delete = async (req, res, service) => {
     const id = req.params.id;
-    await service.delete(id);
-    res.status(204)
+    service.delete(id);
+    res.sendStatus(204);
+  }
+
+  static create = async (req, res, service) => {
+    const object = req.body;
+    const newObject = await service.create(object);
+    res.status(201).json(newObject);
   }
 }
