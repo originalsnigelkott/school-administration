cd791f8d0e1aef0c18c2324b94f4c54ef48abb23
Per Dahlstedt <dahlstedt.per@gmail.com>
2020-10-11 11:41:27 +0200

    Removed models

--- models/Student.js
+++
@@ -1,8 +0,0 @@
-const Person = require("./Person");
-
-module.exports = class Student extends Person {
-  constructor(firstName, lastName, dateOfBirth, nickName) {
-    super(firstName, lastName, dateOfBirth);
-    this.nickName = nickName;
-  }
-}
