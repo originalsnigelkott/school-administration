6588df0b92a2832b84cfeae919605ff2239bced5
Per Dahlstedt <dahlstedt.per@gmail.com>
2020-10-11 10:01:54 +0200

    Removed old code

--- services/StudentService.js
+++ services/StudentService.js
@@ -1,19 +1,5 @@
 const BaseService = require("./BaseService");
-const PersonFactory = require("../core/PersonFactory");
-const { db } = require("../core/DatabaseConnection");
 
 module.exports = class StudentService extends BaseService {
   static defaultTable = "students";
-
-  static async create(person) {
-    const newPerson = PersonFactory.makePerson(
-      person.firstName,
-      person.lastName,
-      person.dateOfBirth,
-      person.nickName
-    );
-    const statement = db.prepare(this.createQueryBuilder(newPerson));
-    statement.run({ ...newPerson });
-    return newPerson;
-  }
 };
--- services/TeacherService.js
+++ services/TeacherService.js
@@ -1,18 +1,5 @@
 const BaseService = require("./BaseService");
-const PersonFactory = require("../core/PersonFactory");
-const { db } = require("../core/DatabaseConnection");
 
 module.exports = class TeacherService extends BaseService {
   static defaultTable = "teachers";
-
-  static async create(person) {
-    const newPerson = PersonFactory.makePerson(
-      person.firstName,
-      person.lastName,
-      person.dateOfBirth
-    );
-    const statement = db.prepare(this.createQueryBuilder(newPerson));
-    statement.run({ ...newPerson });
-    return newPerson;
-  }
 };
